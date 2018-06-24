9cf9b640303cada12a932c5c9fc1d5ac50d3af17
Fangfang Zhang <zhangff@ibobei.cn>
2018-06-24 18:55:13 +0800

    增加 branch_4

---
+++ ZZ/Controller/ForthViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForthViewController.h
+//  ZZ
+//
+//  Created by Fangfang Zhang on 2018/6/24.
+//  Copyright © 2018年 Fangfang Zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ForthViewController : ViewController
+
+@end
---
+++ ZZ/Controller/ForthViewController.m
@@ -0,0 +1,23 @@
+//
+//  ForthViewController.m
+//  ZZ
+//
+//  Created by Fangfang Zhang on 2018/6/24.
+//  Copyright © 2018年 Fangfang Zhang. All rights reserved.
+//
+
+#import "ForthViewController.h"
+
+@interface ForthViewController ()
+
+@end
+
+@implementation ForthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
